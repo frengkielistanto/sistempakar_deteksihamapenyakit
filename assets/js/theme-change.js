8deb62645851ba43fafb32d2b8804ac4b5d2e722
frengki elistanto <freng123elistantobi1@gmail.com>
2022-04-02 22:04:45 +0700

    fix issue

--- assets/js/theme-change.js
+++ assets/js/theme-change.js
@@ -1,6 +1,6 @@
 const toggleSwitch = document.querySelector('.theme-switch input[type="checkbox"]');
 const currentTheme = localStorage.getItem('theme');
-
+document.documentElement.setAttribute('data-theme', 'dark');
 if (currentTheme) {
     document.documentElement.setAttribute('data-theme', currentTheme);
   
